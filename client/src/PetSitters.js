ae294219ec00330d260a941390cf822afa23f35e
Ian Iversen <ianiversen1@gmail.com>
2022-09-14 00:47:13 -0400

    Pet sitters mapped, styled components

--- client/src/PetSitters.js
+++ client/src/PetSitters.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 
 function PetSitters() {
+
+    const [petSitters, setPetSitters]  = useState([])
+
+    useEffect(() => {
+        fetch("/pet_sitters")
+          .then((r) => r.json())
+          .then(setPetSitters);
+      }, []);
+
     return(
-        <div>This is the pet sitters page</div>
+        <div>
+            <h1>This is the pet sitters page</h1>
+            {petSitters.map((petSitter) => (
+                <>
+                    <h2> {petSitter.name} </h2>
+                    <p>{petSitter.description} </p>
+                    <img src={petSitter.image}></img>
+                    <h3> Rating: {petSitter.rating} </h3>
+                </>
+            ))}
+        </div>
+        
     )
 }
 
