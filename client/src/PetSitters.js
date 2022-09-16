f45c027cc386a9bca3921c9279a437bd79e530cc
Ian Iversen <ianiversen1@gmail.com>
2022-09-15 22:05:42 -0400

    pet sitters read/create

--- client/src/PetSitters.js
+++ client/src/PetSitters.js
@@ -12,9 +12,53 @@ function PetSitters() {
           .then(setPetSitters);
       }, []);
 
+
+      const [formData, setFormData] = useState({
+        name: "",
+        description: "",
+        image: "",
+        rating: "",
+    });
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData, [e.target.name]: e.target.value,
+        });
+    };
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        const userCreds = { ...formData };
+
+        function refreshPage() {
+            window.location.reload(true);
+          }
+        
+        fetch("/pet_sitters", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userCreds),
+        }).then(res => res.json())
+        .then(data => console.log(data))
+        .then(refreshPage())
+    }
+
+
+
+
     return(
         <div>
             <h1>This is the pet sitters page</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="name" name="name" value={formData.name} onChange={handleChange}></input>
+                <input type="text" placeholder="description" name="description" value={formData.description} onChange={handleChange}></input>
+                <input type="text" placeholder="image" name="image" value={formData.image} onChange={handleChange}></input>
+                
+                <button>Become a Pet Sitter</button>
+            </form>
             {petSitters.map((petSitter) => (
                 <>
                     <h2> {petSitter.name} </h2>
